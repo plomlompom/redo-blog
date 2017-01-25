ba1a088a6e6aacd9fb4f94433d95ab53c8feca83
Christian Heller <c.heller@plomlompom.de>
2017-01-25 02:47:30 +0100

    Fix buggy add_dir script.

--- add_dir.sh
+++ add_dir.sh
@@ -3,8 +3,8 @@
 set -e
 
 link_files_in_dir () {
-  srcdir="$1"
-  for file in "$srcdir"/* "$srcdir"/.*; do
+  local parent_dir="$1"
+  for file in "$parent_dir"/* "$parent_dir"/.*; do
     if [ -f "$file" ]; then
       set +e
       ln -s "$file"
@@ -15,13 +15,13 @@ link_files_in_dir () {
       dir=$(basename "$file")
       mkdir -p "$dir"
       cd "$dir"
-        link_files_in_dir "$srcdir/$dir"
+        link_files_in_dir "$parent_dir/$dir"
       cd ..
     fi
   done
 }
 
-srcdir=`pwd`/processor
+src_dir=`pwd`/processor
 mkdir -p "$1"
 cd "$1"
-link_files_in_dir "$srcdir"
+link_files_in_dir "$src_dir"
